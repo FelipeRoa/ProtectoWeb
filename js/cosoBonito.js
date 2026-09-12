758f9ceffdc6b86751254229de5be72f66297a92
sebastiantroncoso6 <sebastian.troncoso6@mail.udp.cl>
2026-09-12 20:37:01 -0300

    Add files via upload

---
+++ js/cosoBonito.js
@@ -0,0 +1,11 @@
+const sumoda = document.getElementById('modaDeUsuario');
+const botonJiji = document.getElementById('nuevoUsAdmin');
+const funado = document.getElementById('botCancelin');
+
+botonJiji.addEventListener('click', () => {
+    sumoda.classList.remove('acc-usu');
+});
+
+funado.addEventListener('click', () => {
+    modal.classList.remove('acc-usu'); //AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA ODIOLABIDA
+});
